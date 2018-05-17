bd5cb7af4799ef65db296d8c7456ce345727be96
Vincent Chan <cdzos97@gmail.com>
2018-05-17 17:46:39 +0800

    upgrade package.json

--- examples/default/assert.js
+++
@@ -1,8 +0,0 @@
-
-export function equal(a, b) {
-  return true;
-}
-
-export function deepEqual(a, b) {
-  return true;
-}
--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,5 @@
-import * as assert from "assert";
-
-import { ScopeManager } from "./scopeManager";
-import { Variable } from "./variable";
-import { ModuleManager } from "./moduleManager";
-import { ModuleAnalyser } from "./analyser";
-import { Scope } from "./scope";
-// const version = require("./package.json").version;
-
-const moduleManager = new ModuleManager();
-
-const pluginName = "WebpackDeepScopeAnalysisPlugin";
-
-class WebpackDeepScopeAnalysisPlugin {
-  public apply(compiler: any) {
-    compiler.hooks.compilation.tap(
-      pluginName,
-      (compilation: any, data: any) => {
-        let moduleAnalyser: ModuleAnalyser;
-
-        compilation.hooks.normalModuleLoader.tap(
-          pluginName,
-          (loaderContext: any, module: any) => {
-            moduleAnalyser = new ModuleAnalyser(
-              module.resource,
-              module,
-            );
-
-            module.parser.hooks.program.tap(
-              pluginName,
-              (ast: any) => {
-                if (
-                  !moduleManager.map.has(moduleAnalyser.name)
-                ) {
-                  moduleAnalyser.analyze(ast);
-                  moduleManager.map.set(
-                    moduleAnalyser.name,
-                    moduleAnalyser,
-                  );
-                }
-              },
-            );
-          },
-        );
-      },
-    );
-  }
-}
-
-export {
-  WebpackDeepScopeAnalysisPlugin as default,
-  ModuleAnalyser,
-};
+export * from "./scopeManager";
+export * from "./variable";
+export * from "./moduleManager";
+export * from "./analyser";
+export * from "./scope";
